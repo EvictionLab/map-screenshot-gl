7b423dfce85a122c021b386d9370b4fd336b271b
pjsier <pjsier@gmail.com>
2017-11-02 17:26:16 -0500

    Accepting parameters

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 const mbgl = require('@mapbox/mapbox-gl-native');
+const geoViewport = require('@mapbox/geo-viewport');
 const request = require('request');
 const app = require('express')();
+const scales = require('./scales');
 
 const options = {
     request: (req, callback) => {
         request({
-            url: req.url,
+            url: req.url.replace("%2b", "+"),
             encoding: null,
             gzip: true
         }, (err, res, body) => {
@@ -25,25 +27,66 @@ const options = {
 
                 callback(null, response);
             } else {
-                callback(new Error(JSON.parse(body).message));
+                callback(new Error(body));
             }
         });
     },
     ratio: 1
 };
 
-const map = new mbgl.Map(options);
+function getFillProp(layer, dataProp) {
+    const dataScale = scales.filter(p => p.id === dataProp.split('-')[0])[0];
+    return {
+        property: dataProp,
+        default: dataScale.default,
+        stops: dataScale.fillStops.hasOwnProperty(layer) ? dataScale.fillStops[layer] : dataScale.fillStops['default']
+    };
+}
 
-app.get('/', (req, res) => {
+function processMapStyle(style, layer, dataProp, bubbleProp) {
+    const yearSuffix = +dataProp.substr(-2);
+    const censusYear = Math.floor(yearSuffix / 10) * 10;
+    const censusYearSuffix = censusYear < 10 ? "0" + censusYear : censusYear.toString();
+    const year = yearSuffix + (yearSuffix > 40 ? 1900 : 2000);
+    style.layers = style.layers.map(l => {
+        if (l.id.startsWith(layer)) {
+            l.source = `us-${layer}-${censusYearSuffix}`;
+            l.layout.visibility = 'visible';
+            if (l.id === layer) {
+                l.paint['fill-color'] = getFillProp(layer, dataProp);
+            }
+            if (l.id === `${layer}_null`) {
+                l.filter = ['<', dataProp, 0];
+            }
+            if (l.type === 'circle') {
+                l.paint['circle-radius'].property = bubbleProp;
+                l.paint['circle-color'].property = bubbleProp;
+                l.paint['circle-stroke-color'].property = bubbleProp;
+            }
+        }
+        return l;
+    });
+    return style;
+}
+
+// 48.31, 41.7, -82.1, -90.4
+// Example request: http://localhost:3000/48.31/41.7/-82.1/-90.4/states/p-16/er-16
+app.get('/:n/:s/:e/:w/:layer/:dataProp/:bubbleProp', (req, res) => {
     request({
         url: 'http://eviction-maps.s3-website.us-east-2.amazonaws.com/assets/style.json',
     }, (err, styleRes, body) => {
         if (styleRes.statusCode === 200) {
             const styleBody = JSON.parse(body);
-            map.load(styleBody);
+            const map = new mbgl.Map(options);
+            const style = processMapStyle(styleBody, req.params.layer, req.params.dataProp, req.params.bubbleProp);
+            map.load(processMapStyle(styleBody, req.params.layer, req.params.dataProp, req.params.bubbleProp));
 
-            map.render({ zoom: 3, center: [-98.5556199, 39.8097343] }, (err, buffer) => {
-                if (err) throw err;
+            const mapParams = geoViewport.viewport(
+                [+req.params.w, +req.params.s, +req.params.e, +req.params.n],
+                [256, 256]
+            );
+            map.render(mapParams, (err, buffer) => {
+                if (err) console.error(err);
 
                 map.release();
 
